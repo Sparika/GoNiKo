f53bead750a70bd456efbdea19ff6a8695498de9
Kevin CORRE <kevin1.corre@orange.com>
2017-12-13 14:46:51 +0100

    simplified code and added error code

--- main.go
+++ main.go
@@ -34,18 +34,16 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
     body, _ := ioutil.ReadAll(r.Body)
     var req MSG
     err := json.Unmarshal(body, &req)
-    if(err!=nil){
-      fmt.Fprintf(w, `{"fault":%s}`, err)
-    } else if(req.Login == login && req.Pass == pwd){
+    if(req.Login == login && req.Pass == pwd){
       now := time.Now().Unix()
       token := JWT{req.Login, now, now+3600}
-      res, err := json.Marshal(MSG{Token:&token})
-      if(err!=nil){
-        fmt.Fprintf(w, `{"fault":%s}`, err)
-      } else {
-        fmt.Fprintf(w, `%s`, res)
-      }
+      res, _ := json.Marshal(MSG{Token:&token})
+      w.Write(res)
+    } else if(err!=nil){
+      w.WriteHeader(http.StatusBadRequest)
+      fmt.Fprintf(w, `{"fault":%s}`, err)
     } else {
+      w.WriteHeader(http.StatusUnauthorized)
       fmt.Fprintf(w, `{"fault":"error invalid login and password"}`)
     }
 }
@@ -57,20 +55,24 @@ func computeHandler(w http.ResponseWriter, r *http.Request) {
     body, _ := ioutil.ReadAll(r.Body)
     var req MSG
     err := json.Unmarshal(body, &req)
-    if(err!=nil){
-      fmt.Fprintf(w, `{"fault":%s}`, err)
-    }
     if(req.Token.Sub == "admin" && req.Token.Exp >= time.Now().Unix() && req.Token.Iat <= time.Now().Unix()){
       expr,err:=eval.ParseString(req.Expression,"")
       r,err:=expr.EvalToInterface(nil)
-      if(err!=nil){
-        fmt.Fprintf(w, `{"fault":%s}`, err)
-      } else {
+      if(err==nil){
         res, _ := json.Marshal(MSG{Result:r})
-        fmt.Fprintf(w, `%s`, res)
+        //w.WriteHeader(http.StatusOK)
+        w.Write(res)
+        //fmt.Fprintf(w, `%s`, res)
+      } else if(err!=nil){
+        w.WriteHeader(http.StatusBadRequest)
+        fmt.Fprintf(w, `{"fault":%s}`, err)
       }
+    } else if(err!=nil){
+      w.WriteHeader(http.StatusBadRequest)
+      fmt.Fprintf(w, `{"fault":%s}`, err)
     } else {
-        fmt.Fprintf(w, `{"fault":"invalid token"}`)
+      w.WriteHeader(http.StatusUnauthorized)
+      fmt.Fprintf(w, `{"fault":"invalid token"}`)
     }
 
 }
